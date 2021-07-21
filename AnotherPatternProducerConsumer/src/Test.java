7197a48a40c68d310ee43d8951351fadc35793bc
mhumfrey <apn0597@gmail.com>
2021-07-21 17:40:48 +0300

    WaitAndNotify test

---
+++ AnotherPatternProducerConsumer/src/Test.java
@@ -0,0 +1,51 @@
+import sun.nio.ch.sctp.SctpNet;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        WaitAndNotify wn = new WaitAndNotify();
+        Thread thread1 = new Thread(wn::produce);
+        Thread thread2 = new Thread(wn::consume);
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class WaitAndNotify {
+    public void produce() {
+        synchronized (this) {
+            System.out.println("Producer thread started...");
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Producer thread resumed...");
+        }
+    }
+    public void consume() {
+        Scanner scan = new Scanner(System.in);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        synchronized (this) {
+            System.out.println("Waiting for return key pressed");
+            scan.nextLine();
+            this.notify();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
